ae18c5da30e63f8d3455f672b33d4fa045f67726
JennC <2843274115LL@gmail.com>
2023-08-28 20:53:50 +0800

    伸缩菜单组件拆分

--- src/components/container/src/ConstantVue.vue
+++ src/components/container/src/ConstantVue.vue
@@ -1,37 +1,18 @@
 <script lang="ts" setup>
+import NavHeader from './navHeader/NavHeader.vue'
+import NavSide from './navSide/NavSide.vue'
 import { ref } from 'vue'
 const isCollapse = ref(false)
-let toggle = () => {
-    isCollapse.value = !isCollapse.value
-}
 </script>
 <template>
     <el-container class="el-container">
         <el-aside width="auto">
-            <el-menu 
-                :collapse="isCollapse" 
-                default-active="2" 
-                class="el-menu-vertical-demo"            >
-                <el-menu-item index="1">
-                    <el-icon-menu></el-icon-menu>
-                    <span>导航1</span>
-                </el-menu-item>
-                <el-menu-item index="2">
-                    <el-icon-menu></el-icon-menu>
-                    <span>导航2</span>
-                </el-menu-item>
-                <el-menu-item index="3">
-                    <el-icon-menu></el-icon-menu>
-                    <span>导航3</span>
-                </el-menu-item>
-            </el-menu>
+            <nav-side v-model:collapse="isCollapse" />
         </el-aside>
         <el-container>
             <el-header>
-                <span @click="toggle">
-                    <el-icon-expand v-if="isCollapse"></el-icon-expand>
-                    <el-icon-fold v-else></el-icon-fold>
-                </span>
+                <!-- 通过v-model的方式传值collapse 子组件就能改这个值 -->
+                <nav-header v-model:collapse="isCollapse" />
             </el-header>
             <el-main> <router-view></router-view> </el-main>
             <el-footer>Footer</el-footer>
@@ -39,12 +20,5 @@ let toggle = () => {
     </el-container>
 </template>
 
-<script lang="ts" setup>
 
-</script>
-
-<style lang="scss" scoped>
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px; //菜单不收缩的时候宽度是多少
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/container/src/navSide/NavSide.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-menu :collapse="collapse" default-active="2" class="el-menu-vertical-demo">
+        <el-menu-item index="1">
+            <el-icon-menu></el-icon-menu>
+            <span>首页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+            <el-icon-menu></el-icon-menu>
+            <span>图标选择器</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+            <el-icon-menu></el-icon-menu>
+            <span>趋势标记</span>
+        </el-menu-item>
+    </el-menu>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+    collapse: boolean
+}>()
+</script>
+
+<style lang="scss" scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px; //菜单不收缩的时候宽度是多少
+}
+</style>
