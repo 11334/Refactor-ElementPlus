0de668a95c1e6c6c3fc8db455f0d658280da6fed
JennC <2843274115LL@gmail.com>
2023-08-30 21:45:50 +0800

    完美的省市区三级联动 并提交选择信息

--- src/components/chooseArea/src/ChooseArea.vue
+++ src/components/chooseArea/src/ChooseArea.vue
@@ -15,6 +15,15 @@
 <script lang="ts" setup>
 import { ref, watch } from 'vue'
 import allAreas from '../lib/pca-code.json'
+export interface AreaItem {
+    name: string,
+    code: string,
+    children?: AreaItem[]
+}
+export interface Data {
+    name: string,
+    code: string,
+}
 // 下拉框选择省份的值
 let province = ref<string>('')
 // 下拉框选择城市的值
@@ -25,17 +34,21 @@ let area = ref<string>('')
 let areas = ref(allAreas)
 
 // 城市下拉框的所有的值
-let selectCity = ref<any[]>([])
+let selectCity = ref<AreaItem[]>([])
+
+// 分发事件给父组件
+let emits = defineEmits(['change'])
+
 // 区域下拉框的所有的值
-let selectArea = ref<any[]>([])
+let selectArea = ref<AreaItem[]>([])
 watch(() => province.value, val => {
     if (val) {
         // Array.prototype.find() 是数组中的一个方法,用于找出第一个符合条件的数组成员
         // 由于找到的元素可能为 undefined,所以使用了 可选链运算符 ! 确保一定可以读取children属性
-        let cities = areas.value.find(item => item.code === province.value)!.children
+        let cities = areas.value.find(item => item.code === province.value)!.children!
         selectCity.value = cities
         // console.log(cities);
-        
+
     }
     city.value = ''
     area.value = ''
@@ -44,13 +57,38 @@ watch(() => city.value, val => {
     if (val) {
         // Array.prototype.find() 是数组中的一个方法,用于找出第一个符合条件的数组成员
         // 由于找到的元素可能为 undefined,所以使用了 可选链运算符 ! 确保一定可以读取children属性
-        let area = selectCity.value.find(item => item.code === city.value)!.children
+        let area = selectCity.value.find(item => item.code === city.value)!.children!
         selectArea.value = area
         // console.log(area);
-        
+
     }
-    area.value=''
+    area.value = ''
 })
+
+// 监听选择区域
+watch(() => area.value, val => {
+    if (val) {
+        let provinceData: Data = {
+            code: province.value,
+            name: province.value && allAreas.find(item => item.code === province.value)!.name
+        }
+        let cityData: Data = {
+            code: city.value,
+            name: city.value && selectCity.value.find(item => item.code === city.value)!.name
+        }
+        let areaData: Data = {
+            code: val,
+            name: val && selectArea.value.find(item => item.code === val)!.name
+        }
+        // console.log(provinceData, cityData,areaData); 
+        emits('change', {
+            province: provinceData,
+            city: cityData,
+            area: areaData
+        })
+    }
+})
+
 </script>
 
 <style lang="scss" scoped></style>
--- src/view/chooseArea/ChooseArea.vue
+++ src/view/chooseArea/ChooseArea.vue
@@ -1,9 +1,14 @@
 <template>
-    <m-choose-area></m-choose-area>
+    <m-choose-area @change='changeArea'></m-choose-area>
 </template>
 
 <script lang="ts" setup>
 import mChooseArea from '../../components/chooseArea/src/ChooseArea.vue'
+
+let changeArea = (val: any) => { 
+    console.log(val);
+    
+}
 </script>
 
 <style lang="scss" scoped>
