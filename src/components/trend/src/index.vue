e76d214f6ec7517ea0c25dea4b0e3d603f78522a
JennC <2843274115LL@gmail.com>
2023-09-03 00:42:46 +0800

    动态绑定class的妙用实现颜色反转

--- src/components/trend/src/index.vue
+++ src/components/trend/src/index.vue
@@ -1,16 +1,20 @@
 <template>
     <div class="trend">
         <div class="text">
-            {{ text }}
+            <slot v-if="slots.default"></slot>
+            <div v-else>{{ text }}</div>
         </div>
         <div class="icon">
-            <el-icon-arrowup :style="{color:upIconColor}" v-if="type==='up'"></el-icon-arrowup>
-            <el-icon-arrowdown :style="{color:downIconColor}" v-else></el-icon-arrowdown>
+            <el-icon-arrowup :style="{ color: !reverseColor ? upIconColor : '#f5222d' }"
+                v-if="type === 'up'"></el-icon-arrowup>
+            <el-icon-arrowdown :style="{ color: !reverseColor ? downIconColor : '#52c41a' }" v-else></el-icon-arrowdown>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+// useSlots能够看当前插槽(当前组件)使用了几次
+import { useSlots} from 'vue'
 let props = defineProps({
     // 标记当前趋势是上升(up)还是下降(down)
     type: {
@@ -23,31 +27,44 @@ let props = defineProps({
     */
     text: {
         type: String,
-        default:'文字'
+        default: '文字'
+    },
+    // 颜色反转只在默认的颜色上生效  如果使用了自定义颜色，这个属性就不生效
+    // 这个属性只要绑定在组件上 就会 变为 true  不加在组件上 默认 false
+    // 这个属性加到组件上就说明 我这个组件的 icon 颜色 不能改变了 必须用默认的
+    reverseColor: {
+        type: Boolean,
+        default: false
     },
     // 上升趋势的图标颜色
     upIconColor: {
         type: String,
-        default:'#f5222d'
+        default: '#f5222d'
     },
     // 下降趋势的图标颜色
     downIconColor: {
         type: String,
-        default:'#52c41a'
+        default: '#52c41a'
     }
 })
+
+let slots = useSlots()
+console.log(slots);
+
 </script>
 
 <style lang="scss" scoped>
-.trend{
+.trend {
     display: flex;
     align-items: center;
-    .text{
+
+    .text {
         font-size: 16px;
         margin-right: 4px;
     }
-    .icon{
-        svg{
+
+    .icon {
+        svg {
             width: 1em;
             height: 1em;
         }
--- src/view/trend/index.vue
+++ src/view/trend/index.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
-        <div class="flex">
+        <!-- <div class="flex">
             <div><m-trend text="营业额"></m-trend></div>
             <div><m-trend text="销售额" type="down"></m-trend></div>
         </div>
         <div class="flex">
             <div><m-trend text="营业额" upIconColor="red"></m-trend></div>
             <div><m-trend text="销售额" type="down" downIconColor="blue"></m-trend></div>
-        </div>
+        </div> -->
+
+        <!-- 匿名插槽 slots.default存在-->
+        <!-- <m-trend text="销售业绩">营业额</m-trend> -->
+        <!-- 非匿名插槽 slots.default不存在-->
+        <!-- <m-trend text="销售业绩"></m-trend> -->
+
+        <m-trend text="销售额" reverseColor upIconColor="#ff0"></m-trend>
+        <m-trend text="营业额" type="down"></m-trend>
     </div>
 </template>
 
