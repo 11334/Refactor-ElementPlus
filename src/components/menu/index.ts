97ef60dd86b68ce1083ab79766a5e0374902f69a
JennC <2843274115LL@gmail.com>
2023-09-03 22:05:05 +0800

    完美的递归调用无限层数的菜单

--- src/components/menu/index.ts
+++ src/components/menu/index.ts
@@ -1,12 +1,13 @@
 import { App } from 'vue'
 import menu from './src/index.vue'
-
+import infiniteMenu from './src/menu'
 // 让这个组件可以通过use的形式使用
 export default {
     // 这是 vue.use的使用  要么这是一个函数 函数名是install
     // 要么是一个对象，对象里面的属性叫install
     install(app: App) {
         // 注册为全局组件
-        app.component('m-menu', menu)
+        app.component('m-menu', menu),
+        app.component('m-infinite-menu', infiniteMenu)
     }
 }
--- src/view/menu/index.vue
+++ src/view/menu/index.vue
@@ -1,5 +1,7 @@
 <template>
     <m-menu :data="data1" defaultActive="1"></m-menu>
+    <m-infinite-menu :data="data2" defaultActive="1"></m-infinite-menu>
+
 </template>
 
 <script setup lang="ts">
@@ -25,6 +27,51 @@ let data1 = [
                 icon: 'document',
             }
         ]
+    }
+];
+let data2 = [
+    {
+        name: '导航1',
+        index: '1',
+        icon: 'document'
+    },
+    {
+        name: '导航2',
+        index: '2',
+        icon: 'document'
+    },
+    {
+        name: '导航3',
+        index: '3',
+        icon: 'document',
+        children: [
+            {
+                name: '导航3-1',
+                index: '3-1',
+                icon: 'document',
+                children: [
+                    {
+                        name: '导航3-1-1',
+                        index: '3-1-1',
+                        icon: 'document',
+                        children: [
+                            {
+                                name: '导航3-1-1-1',
+                                index: '3-1-1-1',
+                                icon: 'document',
+                                children: [
+                                    {
+                                        name: '导航3-1-1-1-1',
+                                        index: '3-1-1-1-1',
+                                        icon: 'document',
+                                    }
+                                ],
+                            }
+                        ],
+                    }
+                ],
+            }
+        ],
     }
 ]
 </script>
