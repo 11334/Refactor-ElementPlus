a69510a77a3ea253b5a2ef43a197f43ee339d178
JennC <2843274115LL@gmail.com>
2023-09-09 11:33:18 +0800

    完善表单部分的注释，增加一点点小功能 prefix-icon

--- src/components/form/src/index.vue
+++ src/components/form/src/index.vue
@@ -1,28 +1,39 @@
 <template>
     <!-- validate-on-rule-change 是否在 rules 属性改变后立即触发一次验证 -->
+    <!-- model 表单实例对象  用来统一管理表单的各个选项 -->
     <el-form ref="form" v-if="model" :validate-on-rule-change="false" v-bind="$attrs" :model="model" :rules="rules">
+
         <template v-for="(item, index) in options" :key="index">
 
+            <!-- 1、不是多选按钮选项(checkbox-group)、单选按钮选项(radio-group)等有表单项子元素的表单选项-->
             <el-form-item v-if="!item.children || !item.children!.length" :prop="item.prop" :label="item.label">
-                <!-- component : 表单的什么元素是输入框还是单选按钮还是复选框？  v-model="model[item.prop!] 绑定表单其中一个元素的数据  v-bind="item.attrs" 绑定该表单元素的一些特殊操作比如显示密码、一键清空-->
+
+                <!-- 1、不是上传选项(upload),也不是富文本选项(editor), -->
+                <!-- 2、v-model="model[item.prop!] 绑定表单其中一个元素的数据  -->
+                <!-- 3、v-bind="item.attrs" 绑定该表单元素的一些特殊操作比如显示密码、一键清空-->
+                <!-- 4、component 用来渲染匹配的标签 -->
                 <component v-bind="item.attrs" :is="`el-${item.type}`" v-model="model[item.prop!]"
-                    :placeholder="item.placeholder" v-if="item.type !== 'upload' && item.type !== 'editor'"></component>
+                    :placeholder="item.placeholder" :prefix-icon="item.prefixIcon"
+                    v-if="item.type !== 'upload' && item.type !== 'editor'">
+                </component>
+
+                <!-- 上传选项(upload) -->
                 <el-upload v-if="item.type === 'upload'" v-bind="item.uploadAttrs" :on-preview="onPreview"
                     :on-success="onSuccess" :on-remove="onRemove" :on-error="onError" :on-progress="onProgress"
                     :on-change="onChange" :before-upload="beforeUpload" :before-remove="beforeRemove"
                     :http-request="httpRequest" :on-exceed="onExceed">
-                    <!-- 之前做什么项目  做什么模块！模块功能描述怎么做  爱好  其他学习 重在沟通 -->
-                    <!-- 项目公司随便编 或者切换城市选一个公司 不查  项目问题:第三方包包名叫啥    toB  toC-->
-                    <!-- 八股文   功能实现方案  唠嗑 -->
-                    <!-- 上传文件的载体  点击这个区域触发上传操作-->
+
+                    <!-- name="uploadArea" 上传文件的载体  点击这个区域触发上传操作-->
                     <slot name="uploadArea"></slot>
                     <!-- 上传提示 -->
                     <slot name="uploadTip"></slot>
                 </el-upload>
+
+                <!-- 富文本选项(editor) -->
                 <div id="editor" v-if="item.type === 'editor'"></div>
             </el-form-item>
 
-
+            <!-- 2、多选按钮选项(checkbox-group)、单选按钮选项(radio-group)等有表单项子元素的表单选项-->
             <el-form-item v-if="item.children && item.children.length" :prop="item.prop" :label="item.label">
                 <component :placeholder="item.placeholder" v-bind="item.attrs" :is="`el-${item.type}`"
                     v-model="model[item.prop!]">
@@ -33,8 +44,12 @@
             </el-form-item>
 
         </template>
+
+        <!-- 操作表单的选项 比如提交 重置都能在父组件接收的的插槽中写 -->
         <el-form-item>
-            <!-- 实际上没有办法直接在插槽中触发表单的方法，只能用作用域插槽 获取在这定义的表单实例  通过实例调取表单方法   :form="form" 将form通过作用域插槽分发出去  除了拿到表单实例以外  还要拿到表单的值 :model="model" -->
+            <!-- 1、实际上没有办法直接在插槽中触发表单的方法，只能用作用域插槽 获取表单实例  通过实例调取表单方法操作表单 -->
+            <!-- 2、:form="form" 将form通过作用域插槽分发出去  除了拿到表单实例以外  还要拿到表单的值 :model="model" -->
+            <!-- 3、在父组件接收表单时 <template #action="scope"> 接收一个scope 这个scope名字随意 它接收了form->表单实例对象 还有 model->表单的数据-->
             <slot name="action" :form="form" :model="model"></slot>
         </el-form-item>
     </el-form>
@@ -51,8 +66,9 @@ let emits = defineEmits(["on-preview", "on-success"
     , "on-remove", "on-error", "on-progress", "on-change"
     , "before-upload", "before-remove", 'on-exceed'])
 
+// 定义子组件接受父组件传递的数据的类型
 let props = defineProps({
-    // 表单的配置项
+    // 表单选项的配置项
     options: {
         type: Array as PropType<FormOptions[]>,
         required: true
@@ -63,13 +79,14 @@ let props = defineProps({
     }
 })
 
+// ***************************************************************************************************************************
 // Form 组件提供了表单验证的功能，只需为 rules 属性传入约定的验证规则绑定到el-form，并将 el-form-Item 的 prop 属性设置为需要验证的特殊键值即可(要与rules校验的属性名一致) 
 // :model 为表单数据对象 表示这个表单都提交什么数据
 let model = ref<any>(null)
 let rules = ref<any>(null)
 let form = ref<FormInstance | null>()
 let edit = ref()
-// 初始化表单的方法
+// 初始化表单的方法------搭配watch深度观测-----保证从父组件接收的数据的实时性
 let initForm = () => {
     if (props.options && props.options.length) {
         // 初始值为空
@@ -79,9 +96,10 @@ let initForm = () => {
 
         //从父组件准备好的数据 options中 遍历一个一个的对象 
         props.options.map((item: FormOptions) => {
-            // 将一个对象的prop(唯一标识) 赋值给m和r两个对象当做属性
+            // 将一个对象的prop(唯一标识: username、password等) 赋值给m和r两个对象当做属性
             // item就是option中一个一个完整的对象
-            m[item.prop!] = item.value //m属性名是第n个对象的prop 拿当前对象的value给他赋值  遍历结束会得到 由 所有option对象的value属性(作为m的属性) 组成的对象 m
+            // 通过item.prop 将数据和校验规则关联到一起  他们的属性名是一样的
+            m[item.prop!] = item.value //m属性名是第n个对象的prop 拿当前对象的value给他赋值  遍历结束会得到 由 所有option对象的value属性(作为m的属性值) 组成的对象 m    最后相当于 m{username(prop):"admin"(value) , password(prop):"111111"(value)}
             r[item.prop!] = item.rules //r属性名是第n个对象的prop 拿当前对象的rules给他赋值  遍历结束会得到 由 所有option对象的rules属性(作为r的属性) 组成的对象 r  
             if (item.type === 'editor') {
                 // 初始化富文本
@@ -109,6 +127,15 @@ let initForm = () => {
         rules.value = cloneDeep(r)
     }
 }
+// 初始化表单
+onMounted(() => {
+    initForm()
+})
+// 监听父组件传递进来的options是否改变 实时监听
+watch(() => props.options, () => {
+    initForm()
+}, { deep: true })
+// ******************************************************************************************************************************
 
 // 重置表单的方法
 let resetFields = () => {
@@ -139,13 +166,6 @@ defineExpose({
     getFormDate
 })
 
-onMounted(() => {
-    initForm()
-})
-// 监听父组件传递进来的options是否改变 实时监听
-watch(() => props.options, () => {
-    initForm()
-}, { deep: true })
 
 // 上传组件的所有方法
 let onPreview = (file: File) => {
